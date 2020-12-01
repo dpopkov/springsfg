5c28f257224a5fdd86535c947649b5e8ecab4a5d
Denis Popkov <pkvdenis@gmail.com>
2020-12-01 21:13:19 +0300

    spring5demo: add project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# springsfg
+# springsfg - Spring Framework Guru Projects
+
+## 1 - [spring5demo](spring5demo) - simple demo web application using in-memory database
---
+++ spring5demo/src/test/java/learn/sprb2g/spring5demo/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.sprb2g.spring5demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
