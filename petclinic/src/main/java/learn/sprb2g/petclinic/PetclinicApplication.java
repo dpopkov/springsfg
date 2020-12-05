126b03f4011c48a4053f088ec7eef39521d7748d
Denis Popkov <pkvdenis@gmail.com>
2020-12-05 19:09:54 +0300

    petclinic: create project.

---
+++ petclinic/src/main/java/learn/sprb2g/petclinic/PetclinicApplication.java
@@ -0,0 +1,13 @@
+package learn.sprb2g.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicApplication.class, args);
+    }
+
+}
