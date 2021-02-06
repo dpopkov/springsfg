5879341b098d598a4bab0b4feeb751cedbbbbfed
Denis Popkov <pkvdenis@gmail.com>
2021-02-06 17:29:22 +0300

    rest-client: use URI Component Builder.

--- rest-client/src/main/java/learn/sprb2g/restclient/services/ApiServiceImpl.java
+++ rest-client/src/main/java/learn/sprb2g/restclient/services/ApiServiceImpl.java
@@ -1,8 +1,10 @@
 package learn.sprb2g.restclient.services;
 
 import learn.sprb2g.restclient.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,19 +13,24 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     private final RestTemplate restTemplate;
+    private final String apiUrl;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers() {
-        User[] users = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users", User[].class);
+        User[] users = restTemplate.getForObject(apiUrl, User[].class);
         return users != null ? Arrays.asList(users) : List.of();
     }
 
     @Override
     public User getUser(Long id) {
-        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users/" + id, User.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .pathSegment(id.toString());
+        return restTemplate.getForObject(uriBuilder.toUriString(), User.class);
     }
 }
